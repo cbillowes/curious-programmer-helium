2c2053cbf24de0fb2cae3a68cc4eb3f4422ed077
Clarice Bouwer <cbillowes@gmail.com>
2019-01-05 11:42:50 +0200

    Add tags page

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -1,17 +1,64 @@
-import React, { Component } from "react";
-import Layout from "../layout";
-import Metadata from "../components/Metadata/Page/Metadata";
+import React, { Component } from "react"
+import { graphql } from "gatsby"
+import Layout from "../layout"
+import Metadata from "../components/Metadata/Page/Metadata"
+import Tags from "../components/Tag/Tags"
+const _ = require("lodash")
+import "./tags.scss"
 
 class TagsPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tags: this.getTags()
+    }
+  }
+
+  getTags = () => {
+    let data = this.props.data.allMarkdownRemark.edges
+      .map(item => item.node.frontmatter)
+      .map(item => item.tags)
+    let tagCollection = data.map(item => (!item ? [] : item))
+    let tags = []
+    for (let i = 0; i < tagCollection.length; i++) {
+      for (let j = 0; j < tagCollection[i].length; j++) {
+        let tag = tagCollection[i][j]
+        if (!_.includes(tags, tag)) tags.push(tag)
+      }
+    }
+    return tags.sort()
+  }
+
   render() {
     return (
       <Layout>
-        <div className="tags-container">
+        <div className="tags-container container">
           <Metadata slug="/tags" />
+          <h1>Tags</h1>
+          <Tags tags={this.state.tags} />
         </div>
       </Layout>
-    );
+    )
   }
 }
 
-export default TagsPage;
+export default TagsPage
+
+/* eslint no-undef: "off"*/
+export const pageQuery = graphql`
+  query TagsPage {
+    allMarkdownRemark(limit: 1000) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            tags
+          }
+        }
+      }
+    }
+  }
+`
