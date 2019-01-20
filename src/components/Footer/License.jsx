68c50000a8550f9487f2216e726f80540352bc4b
Clarice Bouwer <cbillowes@gmail.com>
2019-01-20 10:13:20 +0200

    Simplify footer for smaller view ports

--- src/components/Footer/License.jsx
+++ src/components/Footer/License.jsx
@@ -1,23 +1,31 @@
 import React, { Component } from "react"
-import License from "../License/License"
 import Link from "gatsby-link"
+import External from "../Link/ExternalLink"
+import License from "../License/License"
 import "./License.scss"
 
 class LicenseFooter extends Component {
   render() {
     return (
       <div className="license-footer">
-        <License /> 
-        {" "}
-        <p>
-          Also, there is a 
+        <div className="grand">
+          <License />
           {" "}
-          <Link to="/license">License</Link>{" for the code "}
-          and it's probably a good idea to read the
-          {" "}
-          <Link to="/privacy-policy">Privacy Policy</Link>
-          .
-        </p>
+          <p>
+            Also, there is a
+            {" "}
+            <Link to="/license">License</Link>{" for the code "}
+            and it's probably a good idea to read the
+            {" "}
+            <Link to="/privacy-policy">Privacy Policy</Link>
+            .
+          </p>
+        </div>
+        <div className="baby">
+          <Link to="/privacy-policy">Privacy Policy</Link> &bull; { " " }
+          <Link to="/license">License</Link> &bull; { " " }
+          <External to="https://github.com/cbillowes/curious-programmer-helium">GitHub</External>
+        </div>
       </div>
     )
   }
