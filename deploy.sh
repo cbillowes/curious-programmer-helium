c844e0aac009e1aeb2271a5987e1544512b2b1f1
Clarice Bouwer <cbillowes@gmail.com>
2019-01-23 21:18:24 +0200

    Try fix netlify deploy (take 3)
    * Add deploy script for netlify as the images folder for the
      manifest doesn't get created during build time anymore.

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+echo "{ curious programmer }"
+echo "Getting ready to deploy this baby"
+echo "//=\\\=//=\\\=//=\\\=//=\\\=//=\\\=//=\\\="
+
+echo "Goin on a man hunt"
+echo "------------------"
+npm install
+
+echo "Tidy up"
+echo "-------"
+npm run clean
+
+echo "Spooky! Creating ghost directory"
+echo "--------------------------------"
+mkdir -p public/images/logos
+
+echo "Hammer and cement"
+echo "-----------------"
+gatsby build
+
+echo "¯\_(ツ)_/¯"
