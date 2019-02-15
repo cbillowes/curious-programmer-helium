b885667e88e9869fb320ae55f33143cb88a99f35
Clarice Bouwer <cbillowes@gmail.com>
2019-02-15 18:34:04 +0200

    Change the way travis-automerge automerges

--- .travis-automerge.sh
+++ .travis-automerge.sh
@@ -7,29 +7,28 @@ if ! grep -q "$BRANCHES_TO_MERGE_REGEX" <<< "$TRAVIS_BRANCH"; then
     exit 0
 fi
 
-export TAG='build.$TRAVIS_BUILD_NUMBER'
+export TAG="build.$TRAVIS_BUILD_NUMBER"
 
-printf "\nI'm about to clone the repository\n"
 # Since Travis does a partial checkout, we need to get the whole thing
 repo_temp=$(mktemp -d)
 
-printf "Cloning https://github.com/$GITHUB_REPO\n"
+printf "\nGetting the repo at https://github.com/$GITHUB_REPO\n"
 git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
 
+printf "\n"
 # shellcheck disable=SC2164
 cd "$repo_temp"
 
-printf "\nLet's see what branch I'm on\n"
+git checkout "$TRAVIS_BRANCH"
+git fetch --all
 git branch
 
-printf '\nChecking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
+printf "\nMerge %s into %s\n" "$TRAVIS_BRANCH" "$BRANCH_TO_MERGE_INTO"
 git checkout "$BRANCH_TO_MERGE_INTO"
+git merge $TRAVIS_BRANCH
 
-printf '\nTagging branch %s\n' "$TAG"
-git tag "$TAG" -a -m 'Generated tag from TravisCI build' "$TRAVIS_BUILD_NUMBER"
-
-printf '\nMerging %s\n' "$TRAVIS_COMMIT" >&2
-git merge --ff-only "$TRAVIS_COMMIT"
+printf '\nTagging branch with "%s"\n' "$TAG"
+git tag -a $TAG -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
 
 printf '\nPushing to %s\n' "$GITHUB_REPO" >&2
 push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
@@ -38,3 +37,6 @@ printf push_uri
 # Redirect to /dev/null to avoid secret leakage
 git push --tags "$push_uri" "$BRANCH_TO_MERGE_INTO" >/dev/null 2>&1
 git push --tags "$push_uri" :"$TRAVIS_BRANCH" >/dev/null 2>&1
+
+printf "\nCleanup house > Delete temp directory\n"
+rm -rf $repo_temp
