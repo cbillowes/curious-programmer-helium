88192d42557e63ab96f838dff63b3d73f44d254e
Clarice Bouwer <cbillowes@gmail.com>
2019-02-16 07:10:40 +0200

    Print where git is pushing to in travis-automerge

--- .travis-automerge.sh
+++ .travis-automerge.sh
@@ -30,7 +30,7 @@ printf '\nTagging branch with "%s"\n' "$TAG"
 git tag -a $TAG -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
 
 push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
-printf '\nPushing to %s\n' "$push_uri" >&2
+printf "\ngit push $push_uri $BRANCH_TO_MERGE_WITH $TAG"
 # Redirect to /dev/null to avoid secret leakage
 git push $push_uri $BRANCH_TO_MERGE_WITH $TAG
 if [ $? -eq 0 ]; then
