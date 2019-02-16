79ac96e43148f50e0e5e78b695dd65dcceb948ba
Clarice Bouwer <cbillowes@gmail.com>
2019-02-16 04:41:14 +0200

    Amend failure mechnism to .travis-automerge.sh & push to /dev/null

--- .travis-automerge.sh
+++ .travis-automerge.sh
@@ -14,34 +14,36 @@ repo_temp=$(mktemp -d)
 
 printf "\nGetting the repo at https://github.com/$GITHUB_REPO\n"
 git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
+ret_clone=$?
 
 printf "\n"
 # shellcheck disable=SC2164
 cd "$repo_temp"
-
 git checkout "$TRAVIS_BRANCH"
 git fetch --all
 git branch
+ret_checkout=$?
 
 printf "\nMerge %s into %s\n" "$TRAVIS_BRANCH" "$BRANCH_TO_MERGE_INTO"
 git checkout "$BRANCH_TO_MERGE_INTO"
 git merge $TRAVIS_BRANCH
+ret_merge=$?
 
 printf '\nTagging branch with "%s"\n' "$TAG"
 git tag -a $TAG -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+ret_tag=$?
 
 printf '\nPushing to %s\n' "$GITHUB_REPO" >&2
 push_uri="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
-
 # Redirect to /dev/null to avoid secret leakage
-if git push $push_uri $BRANCH_TO_MERGE_WITH $TAG ; then
+git push $push_uri $BRANCH_TO_MERGE_WITH $TAG >/dev/null 2>&1
+ret_push=$?
 
-    printf "\nCleanup house > Delete temp directory\n"
-    rm -rf $repo_temp
+printf "\nCleanup house > Delete temp directory\n"
+rm -rf $repo_temp
+ret_cleanup=$?
 
-else
-
-    echo "\nWell this is a disaster o_O"
-    exit 1
-
+if [[ $ret_clone != 0 || $ret_checkout != 0 || $ret_merge != 0 || $ret_tag != 0 || $ret_push != 0  || $ret_cleanup != 0 ]];then
+    printf "\nWell this is a disaster o_O"
+    (exit 1)
 fi
