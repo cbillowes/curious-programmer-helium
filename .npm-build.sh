ea8d2afbe1aab3039e0deb43bce2e8f2e906271e
Clarice Bouwer <cbillowes@gmail.com>
2019-02-19 15:17:02 +0200

    Add spacing between build blocks

--- .npm-build.sh
+++ .npm-build.sh
@@ -2,17 +2,20 @@ echo "Install missing or upgraded node modules"
 echo "-------------------------------------------------------------------------"
 npm install
 npm audit fix
+echo
 
 if [ $? -eq 0 ] ; then
   echo "Cleaning house"
   echo "-------------------------------------------------------------------------"
   npm run clean
+  echo
 fi
 
 if [ $? -eq 0 ] ; then
   echo "Format"
   echo "-------------------------------------------------------------------------"
   echo "Currently disabled"
+  echo
   #npm run format:js
   #npm run write-good
 fi
@@ -21,6 +24,7 @@ if [ $? -eq 0 ] ; then
   echo "Lint"
   echo "-------------------------------------------------------------------------"
   echo "Currently disabled"
+  echo
   #npm run lint:js
   #npm run lint:md
 fi
@@ -29,12 +33,14 @@ if [ $? -eq 0 ] ; then
   echo "Make ghost directory to satisfy needy gatsby-plugin-manifest"
   echo "-------------------------------------------------------------------------"
   mkdir -p public/images/logos
+  echo
 fi
 
 if [ $? -eq 0 ] ; then
   echo "Build"
   echo "-------------------------------------------------------------------------"
   gatsby build
+  echo
 fi
 
 if [ $? -eq 0 ] ; then
