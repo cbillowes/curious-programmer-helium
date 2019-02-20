41c33dab81db87835f26c453033e6d94bf4da633
Clarice Bouwer <cbillowes@gmail.com>
2019-02-20 03:44:48 +0200

    Actually temporarily disable the build and clone branch to see if the Aborted: not currently on any branch message will die a slow painful unnatural death

--- .npm-build.sh
+++ .npm-build.sh
@@ -1,51 +1,58 @@
-echo "Install missing or upgraded node modules"
-echo "-------------------------------------------------------------------------"
-npm install
-npm audit fix
-echo
+#!/bin/bash -e
 
-if [ $? -eq 0 ] ; then
-  echo "Cleaning house"
-  echo "-------------------------------------------------------------------------"
-  npm run clean
-  echo
-fi
+echo "Temporarily disable this build for faster feedback in TravisCI while I struggle my ass of getting my PRs to work."
 
-if [ $? -eq 0 ] ; then
-  echo "Format"
-  echo "-------------------------------------------------------------------------"
-  echo "Currently disabled"
-  echo
-  #npm run format:js
-  #npm run write-good
-fi
+# echo "Install missing or upgraded node modules"
+# echo "-------------------------------------------------------------------------"
+# npm install
+# echo "NOTICE:"
+# echo "There should only be 4 packages with low severity vulnerabilities!"
+# echo "They are manual fixes. Regex denial of service attacks."
+# echo "The application will not be affected by it."
+# echo
 
-if [ $? -eq 0 ] ; then
-  echo "Lint"
-  echo "-------------------------------------------------------------------------"
-  echo "Currently disabled"
-  echo
-  #npm run lint:js
-  #npm run lint:md
-fi
+# if [ $? -eq 0 ] ; then
+#   echo "Cleaning house"
+#   echo "-------------------------------------------------------------------------"
+#   npm run clean
+#   echo
+# fi
 
-if [ $? -eq 0 ] ; then
-  echo "Make ghost directory to satisfy needy gatsby-plugin-manifest"
-  echo "-------------------------------------------------------------------------"
-  mkdir -p public/images/logos
-  echo
-fi
+# if [ $? -eq 0 ] ; then
+#   echo "Format"
+#   echo "-------------------------------------------------------------------------"
+#   echo "Currently disabled"
+#   echo
+#   #npm run format:js
+#   #npm run write-good
+# fi
 
-if [ $? -eq 0 ] ; then
-  echo "Build"
-  echo "-------------------------------------------------------------------------"
-  gatsby build
-  echo
-fi
+# if [ $? -eq 0 ] ; then
+#   echo "Lint"
+#   echo "-------------------------------------------------------------------------"
+#   echo "Currently disabled"
+#   echo
+#   #npm run lint:js
+#   #npm run lint:md
+# fi
 
-if [ $? -eq 0 ] ; then
-  echo "All good! :D+<"
-else
-  echo "Well now... this is a disaster! o_O"
-fi
+# if [ $? -eq 0 ] ; then
+#   echo "Make ghost directory to satisfy needy gatsby-plugin-manifest"
+#   echo "-------------------------------------------------------------------------"
+#   mkdir -p public/images/logos
+#   echo
+# fi
+
+# if [ $? -eq 0 ] ; then
+#   echo "Build"
+#   echo "-------------------------------------------------------------------------"
+#   gatsby build
+#   echo
+# fi
+
+# if [ $? -eq 0 ] ; then
+#   echo "All good! :D+<"
+# else
+#   echo "Well now... this is a disaster! o_O"
+# fi
 
--- .travis-pull-request.sh
+++ .travis-pull-request.sh
@@ -3,13 +3,17 @@
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "build.$TRAVIS_BUILD_NUMBER"; fi`
 export REPO="https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO"
 
+echo "Cloning repository"
+git clone https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO $TRAVIS_BRANCH
+git fetch
+
 echo "Tagging the build $TAG"
 echo "Working on $TRAVIS_BRANCH: Pull request: $TRAVIS_PULL_REQUEST"
 
 echo "Creating git tag"
 git tag -a $TAG -m "Tagged by TravisCI for $COMMIT"
 
-echo "Create pull request"
+echo "Creating pull request for $COMMIT on $TAG"
 hub pull-request -m "Create PR for $COMMIT on $TAG"
 
 echo "Pushing to GitHub"
