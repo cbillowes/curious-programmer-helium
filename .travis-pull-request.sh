5b1182b13e33a9f41ace8128ae5939f8dd4963bb
Clarice Bouwer <cbillowes@gmail.com>
2019-02-22 06:19:32 +0200

    Use TravisCI issues api instead of PR api
    Where the hell this documentation lives, I don't know. I've not been able to come across it in my
    efforts to search for automated pull requests. Let's see if I can get it to work somehow.

--- .travis-pull-request.sh
+++ .travis-pull-request.sh
@@ -1,73 +1,22 @@
 #!/bin/bash
-set -ev
 
-# TODO: Get rid of $?
-# https://docs.travis-ci.com/user/job-lifecycle/#note-on-
-
-echo "Creating variables"
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo "build.$TRAVIS_BUILD_NUMBER"; fi`
-export REPO="https://$GITHUB_TOKEN@github.com/$GITHUB_REPO"
-export TRAP=0
-
-function get_source_code() {
-    if [ $TRAP == 0 ]; then
-        echo "---------------------------------------------------------"
-        echo "Getting the source code"
-        echo "---------------------------------------------------------"
-
-        git clone https://$GITHUB_TOKEN@github.com/$GITHUB_REPO
-        git branch
-        git checkout $TRAVIS_BRANCH
-        git fetch origin $TRAVIS_BRANCH
-        TRAP=$?
-    fi
-}
-
-function tag_branch() {
-    if [ $TRAP == 0 ]; then
-        echo "---------------------------------------------------------"
-        echo "Tagging branch"
-        echo "---------------------------------------------------------"
-
-        echo "git tag -a $TAG -m \"Tagged by TravisCI for $TRAVIS_COMMIT\""
-        git tag -a $TAG -m "Tagged by TravisCI for $TRAVIS_COMMIT"
-        TRAP=$?
-    fi
-}
-
-function push_tags() {
-    if [ $TRAP == 0 ]; then
-        echo "---------------------------------------------------------"
-        echo "Pushing tags to GitHub"
-        echo "---------------------------------------------------------"
-
-        echo "git push https://$GITHUB_TOKEN@github.com/$GITHUB_REPO origin/$TRAVIS_BRANCH $TAG"
-        git push https://$GITHUB_TOKEN@github.com/$GITHUB_REPO origin/$TRAVIS_BRANCH $TAG
-        TRAP=$?
-    fi
-}
-
-function create_pull_request() {
-    if [ $TRAP == 0 ]; then
-        if [ "$TRAVIS_BRANCH" == "develop" ]; then
-            echo "---------------------------------------------------------"
-            echo "Creating pull request"
-            echo "---------------------------------------------------------"
-            #echo hub pull-request -p -b $TRAVIS_BRANCH -h $TRAVIS_COMMIT -m \"Create PR for $TRAVIS_COMMIT on $TAG"\""
-            #hub pull-request -p -b $TRAVIS_BRANCH -h $TRAVIS_COMMIT -m "Create PR for $TRAVIS_COMMIT on $TAG"
-
-            echo "git request-pull $TAG https://$GITHUB_TOKEN@github.com/$GITHUB_REPO develop"
-            git request-pull $TAG https://$GITHUB_TOKEN@github.com/$GITHUB_REPO develop
-            TRAP=$?
-        fi
-    fi
-}
-
-if [ $TRAVIS_BRANCH == "develop" ]; then
-    get_source_code
-    tag_branch
-    #push_tags
-    create_pull_request
+if [ -n "${TRAVIS_TAG// }" ] #TAG is not empty
+then
+  #sorts the tags and picks the latest
+  #sort -V does not work on the travis machine
+  #sort -V              ref: http://stackoverflow.com/a/14273595/689223
+  #sort -t ...          ref: http://stackoverflow.com/a/4495368/689223
+  #reverse with sed     ref: http://stackoverflow.com/a/744093/689223
+  #git tags | ignore release candidates | sort versions | reverse | pick first line
+  LATEST_TAG=`git tag | grep -v rc | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | sed '1!G;h;$!d' | sed -n 1p`
+  echo $LATEST_TAG
 fi
 
-(exit $TRAP)
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+    # Using the Issues api instead of the PR api
+    # Done so because every PR is an issue, and the issues api allows to post general comments,
+    # while the PR api requires that comments are made to specific files and specific commits
+    GITHUB_PR_COMMENTS=https://api.github.com/repos/${GITHUB_REPO}/issues/${TRAVIS_PULL_REQUEST}/comments
+    curl -H "Authorization: token ${GITHUB_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${LATEST_TAG}'"}'
+fi
