4fa7825276ac8c0db87727fe942cc0fd5d907ef7
Clarice Bouwer <cbillowes@gmail.com>
2019-03-09 09:17:12 +0200

    Hardcode TARGET_BRANCH
    CF value does not seem to take effect

--- .run-pull-request.sh
+++ .run-pull-request.sh
@@ -16,7 +16,7 @@ export URL="${GITHUB_API}/pulls"
 export DESCRIPTION="Build from revision ${CF_REVISION}"
 export VERSION="${CF_PULL_REQUEST_NUMBER}"
 export SOURCE_BRANCH="${CF_BRANCH}"
-export TARGET_BRANCH="${CF_BASE_BRANCH}"
+export TARGET_BRANCH="master"
 export DATA='{"title":"Automated release \uD83D\uDC4F '${VERSION}'", "body":"'${DESCRIPTION}'", "base":"'${TARGET_BRANCH}'","head":"'${SOURCE_BRANCH}'"}'
 
 echo "About to create a pull request posting " \
